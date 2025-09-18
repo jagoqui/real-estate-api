0c0ebc11f2650c297229095c59154acd4e1159a8
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-17 20:37:43 -0500

    Add global error handling middleware

---
+++ Infrastructure/API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,35 @@
+using RealEstate.Application.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace RealEstate.Infrastructure.API.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = ex switch
+                {
+                    BadRequestException => (int)HttpStatusCode.BadRequest,
+                    NotFoundException => (int)HttpStatusCode.NotFound,
+                    InternalServerErrorException => (int)HttpStatusCode.InternalServerError,
+                    _ => (int)HttpStatusCode.InternalServerError
+                };
+
+                var response = new { error = ex.Message };
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+        }
+    }
+
+}
