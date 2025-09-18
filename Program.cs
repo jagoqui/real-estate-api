971babf5a7c4a1560e03f5a7f5f8d778c0675298
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-18 15:04:34 -0500

    Add DotNetEnv and environment variable configuration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using DotNetEnv;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using RealEstate.Infrastructure.Config;
@@ -5,22 +6,38 @@ using RealEstate.Application.Contracts;
 using RealEstate.Infrastructure.API.Services;
 using RealEstate.Infrastructure.API.Repositories;
 using RealEstate.Infrastructure.API.Middlewares;
+using RealEstate.Infrastructure.API.Conventions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string envPath = builder.Environment.IsDevelopment() ? "environments/.env" : string.Empty;
+
+if (!string.IsNullOrEmpty(envPath) && File.Exists(envPath))
+{
+    Env.Load(envPath);
+    Console.WriteLine($"Cargando variables de entorno desde: {envPath}");
+}
+else
+{
+    throw new FileNotFoundException($"The specified environment file does not exist: {envPath}");
+}
 
 // =======================
 // MongoDB Configuration
 // =======================
-builder.Services.Configure<DatabaseSettings>(
-    builder.Configuration.GetSection("DatabaseSettings"));
+string GetEnvOrThrow(string name) =>
+    Environment.GetEnvironmentVariable(name) 
+        ?? throw new InvalidOperationException($"{name} environment variable is not set or is empty.");
+
+builder.Services.Configure<DatabaseSettings>(options =>
+{
+    options.ConnectionString = GetEnvOrThrow("DatabaseSettings__ConnectionString");
+    options.DatabaseName = GetEnvOrThrow("DatabaseSettings__DatabaseName");
+});
 
 builder.Services.AddSingleton<IMongoClient>(sp =>
 {
     var settings = sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
-
-    if (string.IsNullOrEmpty(settings.ConnectionString))
-        throw new InvalidOperationException("MongoDB ConnectionString is not configured!");
-
     return new MongoClient(settings.ConnectionString);
 });
 
@@ -28,10 +45,6 @@ builder.Services.AddScoped(sp =>
 {
     var settings = sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
     var client = sp.GetRequiredService<IMongoClient>();
-
-    if (string.IsNullOrEmpty(settings.DatabaseName))
-        throw new InvalidOperationException("MongoDB DatabaseName is not configured!");
-
     return client.GetDatabase(settings.DatabaseName);
 });
 
