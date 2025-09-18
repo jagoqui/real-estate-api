7cbb6dd362d42a7a3cb1dcb6201c939bda069477
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-18 09:09:15 -0500

    Enable Swagger middleware in all environments

--- Program.cs
+++ Program.cs
@@ -48,11 +48,8 @@ var app = builder.Build();
 // =======================
 // Middleware
 // =======================
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
