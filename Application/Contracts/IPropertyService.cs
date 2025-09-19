24008492a7968e98d4302ef378690ddc9cb7b6e1
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-18 22:19:55 -0500

    Refactor IPropertyService interface to align with updated domain model

--- Application/Contracts/IPropertyService.cs
+++ Application/Contracts/IPropertyService.cs
@@ -1,13 +1,13 @@
-using RealEstate.Infrastructure.DTOs;
+using RealEstate.Domain.Entities;
 
 namespace RealEstate.Application.Contracts
 {
     public interface IPropertyService
     {
-        Task<IEnumerable<PropertyWithOwnerDto>> GetPropertiesAsync(
-            string? name,
-            string? address,
-            decimal? minPrice,
-            decimal? maxPrice);
+        Task<IEnumerable<Property>> GetAllPropertiesAsync();
+        Task<Property?> GetPropertyByIdAsync(string id);
+        Task<Property> AddPropertyAsync(PropertyWithoutId property);
+        Task<Property> UpdatePropertyAsync(string id, PropertyWithoutId property);
+        Task DeletePropertyAsync(string id);
     }
 }
