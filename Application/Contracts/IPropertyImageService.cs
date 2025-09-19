91626b54963f6b9bfe7e1968a5aabe6eacf9ff00
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-19 00:22:01 -0500

    Add method to update property image file in service interface

--- Application/Contracts/IPropertyImageService.cs
+++ Application/Contracts/IPropertyImageService.cs
@@ -8,6 +8,7 @@ namespace RealEstate.Application.Contracts
         Task<PropertyImage?> GetPropertyImageByIdAsync(string id);
         Task<PropertyImage> AddPropertyImageAsync(PropertyImageWithoutId propertyImage);
         Task<PropertyImage> UpdatePropertyImageAsync(string id, PropertyImageWithoutId propertyImage);
+        Task<PropertyImage?> UpdatePropertyImageFileAsync(string idPropertyImage, string base64File);
         Task DeletePropertyImageAsync(string id);
     }
 }
