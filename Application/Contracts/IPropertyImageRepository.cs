1715d48392f8a13569f8f7b5afd72d5b6d587f9c
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-19 00:22:00 -0500

    Add method to update property image file in repository interface

--- Application/Contracts/IPropertyImageRepository.cs
+++ Application/Contracts/IPropertyImageRepository.cs
@@ -8,6 +8,7 @@ namespace RealEstate.Application.Contracts
         Task<PropertyImage?> GetPropertyImageByIdAsync(string id);
         Task<PropertyImage> AddPropertyImageAsync(PropertyImage propertyImage);
         Task<PropertyImage?> UpdatePropertyImageAsync(string id, PropertyImage propertyImage);
+        Task<PropertyImage?> UpdatePropertyImageFileAsync(string idPropertyImage, string base64File);
         Task DeletePropertyImageAsync(string id);
     }
 }
