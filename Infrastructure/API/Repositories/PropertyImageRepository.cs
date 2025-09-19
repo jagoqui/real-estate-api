adf303b3366fe6afe20101218da606138f6ae3e2
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-18 23:02:02 -0500

    Implement PropertyImageRepository

---
+++ Infrastructure/API/Repositories/PropertyImageRepository.cs
@@ -0,0 +1,43 @@
+using MongoDB.Driver;
+using RealEstate.Domain.Entities;
+using RealEstate.Application.Contracts;
+
+namespace RealEstate.Infrastructure.API.Repositories
+{
+    public class PropertyImageRepository : IPropertyImageRepository
+    {
+        private readonly IMongoCollection<PropertyImage> _propertyImages;
+
+        public PropertyImageRepository(IMongoDatabase database)
+        {
+            _propertyImages = database.GetCollection<PropertyImage>("PropertyImages");
+        }
+
+        public async Task<IEnumerable<PropertyImage>> GetPropertyImagesAsync()
+        {
+            return await _propertyImages.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<PropertyImage?> GetPropertyImageByIdAsync(string id)
+        {
+            return await _propertyImages.Find(pi => pi.IdPropertyImage == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<PropertyImage> AddPropertyImageAsync(PropertyImage propertyImage)
+        {
+            await _propertyImages.InsertOneAsync(propertyImage);
+            return propertyImage;
+        }
+
+        public async Task<PropertyImage?> UpdatePropertyImageAsync(string id, PropertyImage propertyImage)
+        {
+            await _propertyImages.ReplaceOneAsync(pi => pi.IdPropertyImage == id, propertyImage);
+            return propertyImage;
+        }
+
+        public async Task DeletePropertyImageAsync(string id)
+        {
+            await _propertyImages.DeleteOneAsync(pi => pi.IdPropertyImage == id);
+        }
+    }
+}
