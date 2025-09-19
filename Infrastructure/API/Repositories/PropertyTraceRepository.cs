90f32a3f3f4fa4b8f891e4cca96164232c0a8197
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-18 23:02:02 -0500

    Implement PropertyTraceRepository

---
+++ Infrastructure/API/Repositories/PropertyTraceRepository.cs
@@ -0,0 +1,32 @@
+using MongoDB.Driver;
+using RealEstate.Domain.Entities;
+using RealEstate.Application.Contracts;
+
+namespace RealEstate.Infrastructure.API.Repositories
+{
+    public class PropertyTraceRepository : IPropertyTraceRepository
+    {
+        private readonly IMongoCollection<PropertyTrace> _propertyTraces;
+
+        public PropertyTraceRepository(IMongoDatabase database)
+        {
+            _propertyTraces = database.GetCollection<PropertyTrace>("PropertyTraces");
+        }
+
+        public async Task<IEnumerable<PropertyTrace>> GetPropertyTracesAsync()
+        {
+            return await _propertyTraces.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<PropertyTrace?> GetPropertyTraceByIdAsync(string id)
+        {
+            return await _propertyTraces.Find(pt => pt.IdPropertyTrace == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<PropertyTrace> AddPropertyTraceAsync(PropertyTrace propertyTrace)
+        {
+            await _propertyTraces.InsertOneAsync(propertyTrace);
+            return propertyTrace;
+        }
+    }
+}
