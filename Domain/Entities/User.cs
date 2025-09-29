77e975c4b15d8a5dfb0a5f36851f32d1036fc33e
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-29 18:05:25 -0500

    Add User entity

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,34 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using RealEstate.Domain.Enums;
+
+namespace RealEstate.Domain.Entities
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; } = null!;
+
+        [BsonElement("name")]
+        public string? Name { get; set; }
+
+        [BsonElement("googleId")]
+        public string? GoogleId { get; set; }
+
+        [BsonElement("passwordHash")]
+        public string? PasswordHash { get; set; }
+
+        [BsonElement("role")]
+        public UserRole Role { get; set; } = UserRole.OWNER;
+
+        [BsonElement("refreshToken")]
+        public string? RefreshToken { get; set; }
+
+        [BsonElement("RefreshTokenExpiryTime")]
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+    }
+}
