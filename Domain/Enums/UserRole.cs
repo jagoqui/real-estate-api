10a7f7909bb37a509490e1b98b90f296c6480bcb
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-29 18:05:25 -0500

    Add UserRole enum

---
+++ Domain/Enums/UserRole.cs
@@ -0,0 +1,15 @@
+namespace RealEstate.Domain.Enums
+{
+    public enum UserRole
+    {
+        /// <summary>
+        /// The user is an owner.
+        /// </summary>
+        OWNER,
+
+        /// <summary>
+        /// The user is an administrator.
+        /// </summary>
+        ADMIN,
+    }
+}
