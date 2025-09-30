fa15043c094d8d0718a5485350916e7c7cb52b30
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-29 22:52:03 -0500

    Add GoogleTokenResponse entity to handle Google token exchange

---
+++ Domain/Entities/GoogleTokenResponse.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+
+namespace RealEstate.Domain.Entities
+{
+    public class GoogleTokenResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; } = null!;
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; } = null!;
+
+        [JsonPropertyName("scope")]
+        public string Scope { get; set; } = null!;
+
+        [JsonPropertyName("id_token")]
+        public string IdToken { get; set; } = null!;
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = null!;
+    }
+}
