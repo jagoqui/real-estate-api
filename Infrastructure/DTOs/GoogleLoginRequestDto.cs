2c7164bdd4e42d3ddfa819735e231c2970e3d324
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-09-29 22:52:04 -0500

    Update existing GoogleLoginRequestDto to use id_token

--- Infrastructure/DTOs/GoogleLoginRequestDto.cs
+++ Infrastructure/DTOs/GoogleLoginRequestDto.cs
@@ -2,8 +2,6 @@ namespace RealEstate.Application.DTOs
 {
     public class GoogleLoginRequestDto
     {
-        public string Email { get; set; } = null!;
-        public string GoogleId { get; set; } = null!;
-        public string? Name { get; set; }
+        public string IdToken { get; set; } = default!;
     }
 }
