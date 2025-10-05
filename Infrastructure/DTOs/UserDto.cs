d5f55acf2a6167c0b9bc007069ea66f2bda59acb
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-05 15:51:39 -0500

    Add PhoneNumber and Bio properties to UserDto

--- Infrastructure/DTOs/UserDto.cs
+++ Infrastructure/DTOs/UserDto.cs
@@ -7,6 +7,8 @@ namespace RealEstate.Application.DTOs
         public string Id { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Bio { get; set; }
         public string? PhotoUrl { get; set; } = null!;
         public string? GoogleId { get; set; }
         public UserRole Role { get; set; }
