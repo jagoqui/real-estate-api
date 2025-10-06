b316a31c83036ce6a75e23ee4cd786fa63dd7301
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-06 03:00:24 -0500

    Make Owner entity properties nullable where applicable

--- Domain/Entities/Owner.cs
+++ Domain/Entities/Owner.cs
@@ -9,19 +9,19 @@ namespace RealEstate.Domain.Entities
         public string Name { get; set; } = null!;
 
         [BsonElement("Address")]
-        public string Address { get; set; } = null!;
+        public string? Address { get; set; } = null!;
 
         [BsonElement("Phone")]
-        public string Phone { get; set; } = null!;
+        public string? Phone { get; set; } = null!;
 
         [BsonElement("Email")]
-        public string Email { get; set; } = null!;
+        public string? Email { get; set; } = null!;
 
         [BsonElement("Photo")]
-        public string Photo { get; set; } = null!;
+        public string? Photo { get; set; } = null!;
 
         [BsonElement("Birthday")]
-        public DateTime Birthday { get; set; }
+        public DateTime? Birthday { get; set; }
 
         [BsonElement("CreatedAt")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
@@ -31,7 +31,7 @@ namespace RealEstate.Domain.Entities
     {
         [BsonRepresentation(BsonType.ObjectId)]
         [BsonElement("UserId")]
-        public string UserId { get; set; } = null!;
+        public string? UserId { get; set; } = null!;
     }
 
     public class Owner : OwnerWithoutId
