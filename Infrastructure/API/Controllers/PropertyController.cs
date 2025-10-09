8be63d0d29bdebc3acaba1b05bd3c95419eb1630
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-08 21:30:36 -0500

    Improve PropertyController API documentation

--- Infrastructure/API/Controllers/PropertyController.cs
+++ Infrastructure/API/Controllers/PropertyController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RealEstate.Application.Contracts;
 using RealEstate.Domain.Entities;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace RealEstate.Infrastructure.API.Controllers
 {
@@ -16,6 +17,7 @@ namespace RealEstate.Infrastructure.API.Controllers
         }
 
         [HttpGet]
+        [SwaggerOperation(Summary = "Retrieves all properties.")]
         [ProducesResponseType(typeof(IEnumerable<Property>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAllProperties()
         {
@@ -24,6 +26,7 @@ namespace RealEstate.Infrastructure.API.Controllers
         }
 
         [HttpGet("{id}")]
+        [SwaggerOperation(Summary = "Retrieves a property by its ID.")]
         [ProducesResponseType(typeof(Property), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetPropertyById(string id)
@@ -35,6 +38,7 @@ namespace RealEstate.Infrastructure.API.Controllers
         }
 
         [HttpGet("owner/{ownerId}")]
+        [SwaggerOperation(Summary = "Retrieves all properties associated with a specific owner ID.")]
         [ProducesResponseType(typeof(IEnumerable<Property>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetPropertiesByOwnerId(string ownerId)
         {
@@ -43,6 +47,7 @@ namespace RealEstate.Infrastructure.API.Controllers
         }
 
         [HttpPost]
+        [SwaggerOperation(Summary = "Creates a new property.")]
         [ProducesResponseType(typeof(Property), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> CreateProperty([FromBody] PropertyWithoutId property)
@@ -52,6 +57,8 @@ namespace RealEstate.Infrastructure.API.Controllers
         }
 
         [HttpPut("{id}")]
+        [SwaggerOperation(Summary = "Updates an existing property by its ID.")]
+        [Consumes("application/json")]
         [ProducesResponseType(typeof(Property), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -64,6 +71,7 @@ namespace RealEstate.Infrastructure.API.Controllers
         }
 
         [HttpDelete("{id}")]
+        [SwaggerOperation(Summary = "Deletes a property by its ID.")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteProperty(string id)
@@ -73,6 +81,7 @@ namespace RealEstate.Infrastructure.API.Controllers
         }
 
         [HttpGet("filter")]
+        [SwaggerOperation(Summary = "Retrieves properties based on optional filters: name, address, minPrice, maxPrice.")]
         [ProducesResponseType(typeof(IEnumerable<Property>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetPropertiesByFilter(
             [FromQuery] string? name = null,
