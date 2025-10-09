29627250422b303c2b5d183510e9d368dcd1ac8f
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-08 21:30:35 -0500

    Add CreateUserAsync and RecoverPasswordAsync to IUserService

--- Application/Contracts/IUserService.cs
+++ Application/Contracts/IUserService.cs
@@ -1,12 +1,16 @@
 using RealEstate.Application.DTOs;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.DTOs;
 
 namespace RealEstate.Application.Services
 {
     public interface IUserService
     {
+        Task<UserDto> CreateUserAsync(UserCreateDto request);
         Task<IEnumerable<UserDto>> GetAllUsersAsync();
         Task<UserDto> GetUserByIdAsync(string userId);
         Task<UserDto> UpdateAsync(string id, UserWithFileDto user);
+        Task<UserDto> RecoverPasswordAsync(RecoverPasswordRequest request);
         Task<bool> DeleteUserAsync(string userId);
     }
 }
