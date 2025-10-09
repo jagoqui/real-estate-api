5626e161c61e6bdb509c1d5a363e8f063292e525
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-08 21:30:37 -0500

    Refactor UserDto to include UserCreateDto

--- Infrastructure/DTOs/UserDto.cs
+++ Infrastructure/DTOs/UserDto.cs
@@ -2,15 +2,24 @@ using RealEstate.Domain.Enums;
 
 namespace RealEstate.Application.DTOs
 {
-    public class UserDto
+    public class UserBaseDto
     {
-        public string Id { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Name { get; set; }
+        public UserRole Role { get; set; } = UserRole.OWNER;
+    }
+
+    public class UserDto : UserBaseDto
+    {
+        public string Id { get; set; } = null!;
         public string? PhoneNumber { get; set; }
         public string? Bio { get; set; }
-        public string? PhotoUrl { get; set; } = null!;
+        public string? PhotoUrl { get; set; }
         public string? GoogleId { get; set; }
-        public UserRole Role { get; set; }
+    }
+
+    public class UserCreateDto : UserBaseDto
+    {
+        public string Password { get; set; } = null!;
     }
 }
