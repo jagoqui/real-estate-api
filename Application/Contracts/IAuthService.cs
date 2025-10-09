6cf0811acb7a8a2570258d1a18ed86129aa785d8
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-08 21:30:34 -0500

    Refactor IAuthService to use UserCreateDto for registration

--- Application/Contracts/IAuthService.cs
+++ Application/Contracts/IAuthService.cs
@@ -4,7 +4,7 @@ namespace RealEstate.Application.Contracts
 {
     public interface IAuthService
     {
-        Task<AuthResponseDto> RegisterAsync(string email, string name, string password);
+        Task<AuthResponseDto> RegisterAsync(UserCreateDto request);
         Task<AuthResponseDto> LoginWithEmailAsync(string email, string password);
         Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
         Task<AuthResponseDto> LoginWithGoogleCodeAsync(string code);
