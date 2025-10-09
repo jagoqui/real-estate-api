e784d8b1247822672ccffe54a0649067aa781a7e
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-10-08 21:30:35 -0500

    Fix IUserRepository interface and add RecoverAsync method

--- Application/Contracts/IUserRepository.cs
+++ Application/Contracts/IUserRepository.cs
@@ -5,11 +5,13 @@ namespace RealEstate.Application.Contracts
 {
     public interface IUserRepository
     {
+        Task<User> CreateAsync(User user);
         Task<User?> GetByEmailAsync(string email);
         Task<User?> GetByGoogleIdAsync(string googleId);
         Task<User?> GetByIdAsync(string id);
-        Task<User> CreateAsync(User user);
         Task<User?> UpdateAsync(UserDto user);
+
+        Task<User?> RecoverAsync(string userId, string email, string newPasswordHash);
         Task<IEnumerable<User>> GetAllAsync();
         Task DeleteAsync(string userId);
 
