b609fdb8c3e648c37f4946e66f78982e1ca54bff
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-02 18:16:41 -0500

    Add IdOwner property to PropertyRequestDto

--- Infrastructure/DTOs/PropertyRequestDto.cs
+++ Infrastructure/DTOs/PropertyRequestDto.cs
@@ -5,6 +5,7 @@ namespace RealEstate.Infrastructure.DTOs
 {
     public class PropertyRequestDto : PropertyBase
     {
+        public new string IdOwner { get; set; } = null!;
         public new string Status { get; set; } = nameof(PropertyStatus.AVAILABLE);
         public new string Type { get; set; } = nameof(PropertyTypes.OTHER);
     }
