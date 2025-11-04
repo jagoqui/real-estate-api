0d55817e43014eb09c617186069b0373e92c0484
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-03 22:59:18 -0500

    Refactor Location entity to remove MongoDB annotations

--- Domain/Entities/Location.cs
+++ Domain/Entities/Location.cs
@@ -1,13 +1,9 @@
-using MongoDB.Bson.Serialization.Attributes;
-
 namespace RealEstate.Domain.Entities
 {
     public class Location
     {
-        [BsonElement("Lat")]
         public double Lat { get; set; }
 
-        [BsonElement("Lon")]
         public double Lon { get; set; }
     }
 }
