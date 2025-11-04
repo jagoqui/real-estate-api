24920b8ba1b7c388c23c688e02a00679415881b2
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-03 22:59:18 -0500

    Add Amenity entity

---
+++ Domain/Entities/Amenity.cs
@@ -0,0 +1,9 @@
+namespace RealEstate.Domain.Entities
+{
+    public class Amenity
+    {
+        public string Name { get; set; } = null!;
+
+        public string Icon { get; set; } = null!;
+    }
+}
