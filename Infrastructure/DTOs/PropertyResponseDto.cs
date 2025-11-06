65abce7afd8e024f6d612a6aa647e123d333ae80
Jaidiver Gómez Quintero <jagoqui.gomez@gmail.com>
2025-11-06 15:27:25 -0500

    Add PropertyResponseDto class to represent property data in API responses

--- Infrastructure/DTOs/PropertyResponseDto.cs
+++ Infrastructure/DTOs/PropertyResponseDto.cs
@@ -1,12 +1,16 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace RealEstate.Infrastructure.DTOs
 {
-    public class PropertyResponseDto
+    public class PropertyResponseDto : PropertyRequestDto
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = null!;
-        public string IdOwner { get; set; } = null!;
-        public string Name { get; set; } = null!;
-        public string Address { get; set; } = null!;
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; } = null!;
+        public string CodeInternal { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow!;
+        public new List<string> Images { get; set; } = new();
     }
 }
